6353d99d53038ee34e505e8a3881d795ab810b44
Roger Panosso <rogerninopa@gmail.com>
2020-11-27 10:57:44 -0300

    Inicializando

---
+++ headers.php
@@ -0,0 +1,13 @@
+<?php
+		// Configurações header para forçar o download de documentos em excel
+		header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+		header ("Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT");
+		header ("Cache-Control: no-cache, must-revalidate");
+		header ("Pragma: no-cache");
+		header ("Content-type: application/x-msexcel");
+		header ("Content-Disposition: attachment; filename=\"{$arquivo}\"" );
+		header ("Content-Description: PHP Generated Data" );
+		// Envia o conteúdo do arquivo
+		echo $html;
+		exit;
+?>
